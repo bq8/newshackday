7383d91f8f574900509948fa2bef1ce39b89b1c5
got2bq <bquach88@gmail.com>
2014-10-18 16:42:39 +0900

    Need to branch

---
+++ parse/cloud/config/config.js
@@ -0,0 +1,8 @@
+var config = {};
+
+config.NIKKEI = {
+  APP_ID: '', 
+  BASEURL: 'http://dac2.snnm.net:80/api/'
+};
+
+module.exports = config;
--- parse/cloud/models/article.js
+++ parse/cloud/models/article.js
@@ -1,7 +1,27 @@
+var config = require('cloud/config/config');
+
 var Article = Parse.Object.extend('Article', {
   // Instance methods
 }, {
   // Class methods
+});
+
+Parse.Cloud.httpRequest({
+  url: config.NIKKEI.BASEURL + 'xsearch?',
+  params: {
+    keyword: '',
+    fields: ''
+  }
+  headers: {
+    'X-Nikkei-Application-Id': config.NIKKEI.APP_ID
+  },
+  success: function(httpResponse) {
+
+  },
+  error: function(httpResponse) {
+    console.error('Request failed with response code ' + httpResponse.status);
+
+  }
 });
 
 module.exports = Article;
