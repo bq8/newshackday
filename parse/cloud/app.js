78cb0760d20418caaf7b53dfd522fbb884738cea
got2bq <bquach88@gmail.com>
2014-10-25 11:12:03 +0900

    Temp

--- parse/cloud/app.js
+++ parse/cloud/app.js
@@ -1,4 +1,17 @@
+var ArticleHandler = require('cloud/controllers/article');
 
 Parse.Cloud.define('testmain', function(request, response) {
   console.log('Hello world!');
+});
+
+/*
+ * getArtcile
+ *
+ * Returns an entire article.
+ *
+ * Expected input (in request.params):
+ *   articleId      : String, the objectId of the item
+ */
+Parse.Cloud.define('getArticle', function(request, response) {
+
 });
---
+++ parse/cloud/controllers/article.js
@@ -0,0 +1,13 @@
+var ArticleHandler = (function() {
+  
+  // Returns an entire article
+  function getArticle(req, res) {
+
+  }
+
+  return {
+    getArticle: getArticle
+  };
+})();
+
+module.exports = ArticleHandler;
